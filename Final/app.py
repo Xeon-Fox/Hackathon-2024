76f58d74d96fcf22adecde6236dca0e2c4fe72a6
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2024-09-25 12:37:53 +0600

    hi

--- Final/app.py
+++ Final/app.py
@@ -1,43 +1,3 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# import os
-# from enhance import enhance_image
-# from recognition import recognize_text
-# import requests
-# from bs4 import BeautifulSoup
-
-# app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = 'static/uploads'
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/upload', methods=['POST'])
-# def upload_image():
-#     if 'file' not in request.files:
-#         return redirect(request.url)
-#     file = request.files['file']
-#     if file.filename == '':
-#         return redirect(request.url)
-#     if file:
-#         image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#         file.save(image_path)
-
-#         enhanced_image_path = enhance_image(image_path)
-
-
-#         # response = requests.get("http://127.0.0.1:5000")
-#         # soup = BeautifulSoup(response.content, 'html.parser')
-
-#         # element = soup.find(id="language")
-#         # value = element.get("value")
-
-#         recognized_text = recognize_text(enhanced_image_path)
-
-#         return render_template('result.html', image_path=enhanced_image_path, text=recognized_text)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from enhance import enhance_image
@@ -70,6 +30,8 @@ def upload_image():
         recognized_text = recognize_text(enhanced_image_path)
 
         return render_template('result.html', image_path=enhanced_image_path, text=recognized_text)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
